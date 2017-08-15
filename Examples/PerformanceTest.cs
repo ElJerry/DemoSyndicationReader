1798923b292930f12ba991cbfa0373ddaa9c1e57
Gerardo Hurtado Lozano <kanegerardo_@hotmail.com>
2017-08-14 17:07:59 -0700

    Nuevos Ejemplos performance test

---
+++ Examples/PerformanceTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Examples
+{
+    class PerformanceTest
+    {
+        public static async Task RunTest()
+        {
+            Stopwatch sw1 = new Stopwatch();
+
+            sw1.Start();
+            int repetitions = 100000; //5
+
+            var attrs = new List<SyndicationAttribute>()
+            {
+                new SyndicationAttribute("xmlns:content", "http://contoso.com/"),
+            };
+
+            var content = new SyndicationContent("content:hello", "http://contoso.com/", "world");
+            var c1 = new SyndicationContent("hello", "world");
+
+            for (int i = 0; i < repetitions; ++i)
+            {
+                StringWriter sw = new StringWriter();
+
+                //using (XmlWriter xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { }))
+                using (XmlWriter xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { NamespaceHandling = NamespaceHandling.OmitDuplicates }))
+                {
+                    //var writer = new Rss20FeedWriter(xmlWriter, attrs, new Rss20Formatter(attrs, xmlWriter.Settings));
+                    var writer = new Rss20FeedWriter(xmlWriter, null, new OldFormatter());
+
+                    for (int j = 0; j < 10; ++j) //500000
+                    {
+                        await writer.Write(content);
+                        await writer.Write(c1);
+                    }
+
+                    xmlWriter.Flush();
+                }
+
+                //string res = sw.ToString();
+                sw.Dispose();
+            }
+
+            sw1.Stop();
+
+            Console.WriteLine(sw1.ElapsedMilliseconds);
+        }
+    }
+}
--- Examples/Program.cs
+++ Examples/Program.cs
@@ -2,8 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Examples
 {
@@ -13,11 +19,16 @@ namespace Examples
         {
             Console.WriteLine("Syndication Feed Examples");
 
-            Task t;
+            //Task t;
+            //////t = RssWriteItemWithCustomElement.WriteCustomItem();
+            //////t = AtomFeedReaderExample.ReadAtomFeed(@"C:\git\wcf\lab\src\Microsoft.SyndicationFeed\tests\TestFeeds\simpleAtomFeed.xml");
+            //////t = ReadRssItemWithCustomFields.ReadFeed(@"Feeds /rss20-2items.xml");
             //t = RssWriteItemWithCustomElement.WriteCustomItem();
-            //t = AtomFeedReaderExample.ReadAtomFeed();
-            
+            //////t = ReadRssItemWithCustomFields.ReadFeed(@"C:\git\wcf\lab\src\Microsoft.SyndicationFeed\tests\TestFeeds\rss20-2items.xml");
             //Task.WaitAll(t);
+
+            PerformanceTest.RunTest().Wait();
+
         }
     }
 }
